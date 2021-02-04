8f3362ce630a4b50a0a34fc0d660abdc3132aa06
guzin8 <kanburogluguzin@gmail.com>
2021-02-03 19:21:46 -0800

    Add project files.

---
+++ KaratsubaAlgoritmasi/Hesaplama.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaratsubaAlgoritmasi
+{
+    class Hesaplama
+    {
+        public long Us(long sayi, long üs)
+        {
+            for (int i = 1; i <= üs; i++)
+            {
+                sayi *= sayi;
+            }
+            return sayi;
+        }
+    }
+}
---
+++ KaratsubaAlgoritmasi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KaratsubaAlgoritmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int B = 10;
+            Console.WriteLine("Kac basamakli sayilar gireceksiniz(3 den büyük): ");
+            long n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İki tane "+  n + " basamakli sayi giriniz: ");
+            long x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Hesaplama hesaplama = new Hesaplama();
+            long k = hesaplama.Us(B, n - 2);
+
+            long l = hesaplama.Us(B, 2 * (n - 2));
+
+            long x1 = x / k;
+            long y1 = y / k;
+            long x0 = x - x1;
+            long y0 = y - y1;
+            long z0 = x0 * y0;
+            long z2 = x1 * y1;
+            long z1 = (x1 + x0) * (y1 - y0) - (z2 - z0);
+            long p = z2 * l * (n - 2) + z1 * k + z0;
+
+            Console.WriteLine("Sonuc: " + p);
+        }
+    }
+}
